40a4a2d6f1e8e4be60f58a216530aeb2b9480996
Voodoo <killer4815@mail.ru>
2020-10-30 20:17:00 +0300

    Initial commit

---
+++ src/test/kotlin/HelloKotlin/HelloKotlin/HelloKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package HelloKotlin.HelloKotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HelloKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
